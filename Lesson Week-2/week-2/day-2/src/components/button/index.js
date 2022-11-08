0e43b12a9a8d4775c7fc91a5a0a7220d07890752
Gökhan GÜNEŞ <66802027+gokhangns@users.noreply.github.com>
2022-11-08 12:36:59 +0300

    Delete Lesson Week-2 directory

--- Lesson Week-2/week-2/day-2/src/components/button/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Button(props) {
-	const onClick = () => {
-		console.log("tıklandı");
-	};
-	return <button onClick={props.onClick || onClick}>{props.children}</button>;
-}
